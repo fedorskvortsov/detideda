ee16b1ea63f2ac680ee6e1e957e33df58b96bb21
Fedor <skvortsovfk@gmail.com>
2021-08-16 23:20:26 +0300

    style: update style and add some info to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,18 @@ import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import "./index.scss";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Рок-группа" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div className="large-padding text-center">
+      <h1 className="page-title display-4">D e t i D e d a</h1>
+      <p className="lead">
+        Рок-группа <strong>DetiDeda</strong> &mdash; это авторские песни, уютная
+        атмосфера и глубокая философия!
+      </p>
+    </div>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,7 +25,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/about/">Go to page 2</Link> <br />
+      <Link to="/about/">Go to About page</Link> <br />
     </p>
   </Layout>
 );
