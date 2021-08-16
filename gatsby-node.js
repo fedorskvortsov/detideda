1e40fe1848c8601606f5b4c10ad6485c8c6d130e
Fedor <skvortsovfk@gmail.com>
2021-08-16 20:35:19 +0300

    build: add absolute imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+// eslint-disable-next-line no-undef, no-unused-vars
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      // eslint-disable-next-line no-undef
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  });
+};
