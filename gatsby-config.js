1f1744a61b2e88b02eb61121d049965e742a4e16
Fedor <skvortsovfk@gmail.com>
2021-08-16 22:31:56 +0300

    fix: update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,27 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Detideda`,
+        short_name: `Detideda`,
+        description: `The official Detideda website.`,
+        lang: `ru`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ], // Add or remove icon sizes as desired
       },
     },
     `gatsby-plugin-gatsby-cloud`,
